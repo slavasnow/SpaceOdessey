e4b1fd42c99f3b780f5dfd68297d93120b994665
slavasnow <slavash21@yandex.ru>
2022-12-22 22:16:25 +0300

    добавлена генерация псевдослучайных числел

--- ather/main.gd
+++ ather/main.gd
@@ -3,22 +3,18 @@ extends Node2D
 var pointposition = Vector2()
 var rnd = RandomNumberGenerator.new()
 
-var array = []
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for i in range(0,100):
+	rnd.randomize()
+	for i in 200:
 		var asteroid = load("res://Enamis/Asteroid/Asteroid.tscn").instance()
 		pointposition.x = float(rnd.randi_range(-9000,9000))
 		pointposition.y = float(rnd.randi_range(-9000,9000))
 		asteroid.position = pointposition
 		add_child(asteroid)
 
-# удаление элемента из списока или массива
-func dell_array():
-	pass
 # добавление элемента в список или массив
-func add_array():
+func add_asteroid():
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
